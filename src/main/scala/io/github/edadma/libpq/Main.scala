2596a412b844195c9ced490d05010bae74031eb4
Edward A Maxedon Sr <edadma@gmail.com>
2022-08-08 18:30:47 -0400

    test examples

--- src/main/scala/io/github/edadma/libpq/Main.scala
+++ src/main/scala/io/github/edadma/libpq/Main.scala
@@ -3,11 +3,72 @@ package io.github.edadma.libpq
 @main def run(): Unit =
 //  println(s"version of libpq: $libVersion")
 
+//  val conn = connectdb("dbname=postgres user=postgres password=docker host=localhost")
+//
+//  if conn.status == ConnStatus.BAD then
+//    println(s"connection failed ${conn.errorMessage}")
+//    conn.finish()
+//    sys.exit(1)
+//
+//  println(s"server version: ${conn.serverVersion}")
+//  conn.finish()
+
+//  val conn = connectdb("dbname=postgres user=postgres password=docker host=localhost")
+//
+//  if conn.status == ConnStatus.BAD then
+//    println(s"connection failed ${conn.errorMessage}")
+//    conn.finish()
+//    sys.exit(1)
+//
+//  def exec(query: String): Unit =
+//    val res = conn.exec(query)
+//
+//    if res.status != ExecStatus.COMMAND_OK then
+//      println(conn.errorMessage)
+//      res.clear()
+//      conn.finish()
+//      sys.exit(1)
+//
+//    res.clear()
+//
+//  exec("DROP TABLE IF EXISTS Cars")
+//  exec("""
+//      |CREATE TABLE Cars (
+//      | Id INTEGER PRIMARY KEY,
+//      | Name VARCHAR(20),
+//      | Price INT
+//      |)
+//      |""".stripMargin)
+//  exec("INSERT INTO Cars VALUES(1,'Audi',52642)")
+//  exec("INSERT INTO Cars VALUES(2,'Mercedes',57127)")
+//  conn.finish()
+
   val conn = connectdb("dbname=postgres user=postgres password=docker host=localhost")
 
   if conn.status == ConnStatus.BAD then
     println(s"connection failed ${conn.errorMessage}")
     conn.finish()
+    sys.exit(1)
 
-  println(s"server version: ${conn.serverVersion}")
+  def exec(query: String): Unit =
+    val res = conn.exec(query)
+
+    if res.status != ExecStatus.COMMAND_OK then
+      println(conn.errorMessage)
+      res.clear()
+      conn.finish()
+      sys.exit(1)
+
+    res.clear()
+
+  exec("DROP TABLE IF EXISTS Cars")
+  exec("""
+      |CREATE TABLE Cars (
+      | Id INTEGER PRIMARY KEY, 
+      | Name VARCHAR(20), 
+      | Price INT
+      |)
+      |""".stripMargin)
+  exec("INSERT INTO Cars VALUES(1,'Audi',52642)")
+  exec("INSERT INTO Cars VALUES(2,'Mercedes',57127)")
   conn.finish()
